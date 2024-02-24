ecc702ba0fe838e5c710fa0115d1c29d5a95a10e
Shel Valdés <hola@shelvaldes.com>
2024-02-24 15:20:20 -0600

    Bug fixes and retouch

--- src/EmptyAndError/EmptyTodos.js
+++ src/EmptyAndError/EmptyTodos.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import './EmptyAndError.css'
 import { ReactComponent as NoteSVG } from './note.svg'
+import { TodoContext } from "../TodoContext/TodoContext";
 
 function EmptyTodos() {
-    return(
+    const { searchValue } = useContext(TodoContext);
+
+    return (
         <div className="empty-container">
-            <NoteSVG className="empty-svg-illustration"/>
+            <NoteSVG className="empty-svg-illustration" />
             <h3>Todo tranquilo por aquí</h3>
-            <p>Añade una tarea</p>
+
+            {
+                searchValue ? <p>No tenemos esa tarea</p> : <p>Añade una tarea</p>
+            }
+
+
         </div>
     );
 }
